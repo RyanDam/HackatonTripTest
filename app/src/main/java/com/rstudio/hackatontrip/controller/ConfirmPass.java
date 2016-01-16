cd1f8861c260f0438d6243dfc051e4c92f598288
Phi Ánh <phianhnpa1995@gmail.com>
2016-01-16 14:00:54 +0700

    login/signup

--- app/src/main/java/com/rstudio/hackatontrip/controller/ConfirmPass.java
+++ app/src/main/java/com/rstudio/hackatontrip/controller/ConfirmPass.java
@@ -1,15 +1,66 @@
 package com.rstudio.hackatontrip.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.SignUpCallback;
 import com.rstudio.hackatontrip.R;
+import com.rstudio.hackatontrip.model.User;
 
 public class ConfirmPass extends AppCompatActivity {
+
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_pass);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getBundleExtra(RegisterActivity.BUNDLE_KEY);
+
+        username = bundle.getString(RegisterActivity.USERNAME_KEY);
+        password = bundle.getString(RegisterActivity.PASSWORD_KEY);
+        Log.d("TEST", username + " " + password);
+        init();
+    }
+
+    private void init() {
+        Button confirm = (Button) findViewById(R.id.comfirm_bt);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView confirmTextView = (TextView) findViewById(R.id.user_pass);
+                String passwordConfrim = confirmTextView.getText().toString();
+                Log.d("TEST", passwordConfrim);
+                if (passwordConfrim.compareTo(ConfirmPass.this.password) == 0) {
+                    User user = new User();
+                    user.setUsername(username);
+                    user.setPassword(password);
+
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Intent intent = new Intent(ConfirmPass.this, FavotiteActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(ConfirmPass.this,"Some error occur", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(ConfirmPass.this, "Password Confirm not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/rstudio/hackatontrip/controller/LoginActivity.java
+++ app/src/main/java/com/rstudio/hackatontrip/controller/LoginActivity.java
@@ -1,9 +1,23 @@
 package com.rstudio.hackatontrip.controller;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 import com.rstudio.hackatontrip.R;
+import com.rstudio.hackatontrip.model.User;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +26,54 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        init();
+    }
+
+    protected void init() {
+        Button loginBt = (Button) findViewById(R.id.login_bt);
+        TextView registerText = (TextView) findViewById(R.id.new_acc_bt);
+        // set litener of login button
+        loginBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!LoginActivity.this.checkConnection()) {
+                    Toast.makeText(LoginActivity.this, "Check your connection", Toast.LENGTH_SHORT).show();
+                } else {
+                    String username = ((EditText)findViewById(R.id.user_email)).getText().toString();
+                    String password = ((EditText)findViewById(R.id.user_pass)).getText().toString();
+                    // login
+                    User.logInInBackground(username, password, new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            if (user != null) {
+                                // start StartActivity when login done
+                                Intent intent = new Intent(LoginActivity.this, ShowActivity.class);
+                                startActivity(intent);
+                                Log.d("LOGIN", "done");
+                            } else
+                                Toast.makeText(LoginActivity.this, "Invalid username/password", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
+        // set litener of register TextView
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * check network state
+     * @return true if online else false
+     */
+    private boolean checkConnection() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 }

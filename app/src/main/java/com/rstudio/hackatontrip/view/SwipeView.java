2a48ff1749e05cce11d528ea04d568c8c7c5ff50
RyanDam <py.ryan.dam@gmail.com>
2016-01-16 11:06:14 +0700

    dsf

---
+++ app/src/main/java/com/rstudio/hackatontrip/view/SwipeView.java
@@ -0,0 +1,13 @@
+package com.rstudio.hackatontrip.view;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Ryan on 1/16/16.
+ */
+public class SwipeView extends View {
+    public SwipeView(Context context) {
+        super(context);
+    }
+}
